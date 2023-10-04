b58627b5327f64a42a3be595dd8b5c38b56b2ab8
luke <louieo@gmail.com>
2023-10-05 05:53:34 +1100

    Change NotifyMethod to NotifierType and add ping package in Dockerfile

--- MempoolBot.Lib/Common/Settings.cs
+++ MempoolBot.Lib/Common/Settings.cs
@@ -2,7 +2,7 @@
 
 public class Settings
 {
-    public required string NotifyMethod { get; set; }
+    public required string NotifierType { get; set; }
     public required string MempoolApiUrl { get; set; }
     public required int EconomyRateThreshold { get; set; }
     public required int NotifyRepeatFrequencyMinutes { get; set; }
